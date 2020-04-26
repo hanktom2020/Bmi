947c42b3d51fda3ccae16b8ea0c9cd0dc02a5318
Hank Tom <hanktom@gmail.com>
2020-04-26 13:46:56 +0800

    layout, find views, onclick

--- app/src/main/java/com/tom/bmi/MainActivity.java
+++ app/src/main/java/com/tom/bmi/MainActivity.java
@@ -2,12 +2,35 @@ package com.tom.bmi;
 
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
+
+    private EditText edWeight;
+    private EditText edHeight;
+    private Button bHelp;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        findViews();
+    }
+    public void bmi(View v) {
+        String w = edWeight.getText().toString();
+        String h = edHeight.getText().toString();
+        float weight = Float.parseFloat(w);
+        float height = Float.parseFloat(h);
+        float bmi = weight / (height * height);
+        Log.d("BMI", String.valueOf(bmi));
+    }
+
+    private void findViews() {
+        edWeight = findViewById(R.id.ed_weight);
+        edHeight = findViewById(R.id.ed_height);
+        bHelp = findViewById(R.id.b_help);
     }
 }
